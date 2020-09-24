1d932bfc823e4c18292cf85f3dfd08d86132f3a7
KUTlime <kutlime@gmail.com>
2020-09-24 21:23:15 +0200

    Add CI/CD

---
+++ PublishModule.ps1
@@ -0,0 +1,16 @@
+$psGet = 'PowerShellGet'
+$nugetApiKey = $args[0]
+$minimalPSGetVersion = [System.Version]'2.2.3'
+if ($nugetApiKey)
+{
+    Write-Host "Api key exist."
+}
+
+Import-Module PowerShellGet
+$psGetModule = Get-Module | Where-Object { $_.Name -eq $psGet }
+if ($psGetModule.Version -lt $minimalPSGetVersion)
+{
+    Install-Module -Name $psGet -MinimumVersion '2.2.3'
+}
+
+Publish-Module -Name "$PSScriptRoot\GoogleImageSearch\" -NuGetApiKey $nugetApiKey
